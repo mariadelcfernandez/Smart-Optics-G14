2cc8ead47d7cfc171538871e3350e70dd819a82a
Mary <kennel.liebig@gmail.com>
2024-07-06 18:48:41 -0300

    Add files via upload
    Actualizar Back

---
+++ app.js
@@ -0,0 +1,7 @@
+import Server from "./server/Server.js";
+Server.run(process.env.PORT || 8080)
+//const Puerto = process.env.ALWAYSDATA_HTTPD_PORT
+//const ipAddr = process.env.ALWAYSDATA_HTTPD_ip
+//Server.run(Puerto, ipAddr)
+
+//console.log('args: ', process.argv)
